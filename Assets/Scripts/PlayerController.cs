3d21790cfecf2d2585b2a146415a0a3e8bdc8903
EQwon <bluetwintail@naver.com>
2017-02-05 04:55:25 +0900

    일단 플레이어가 움직이긴 함

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovingSpeed;
+    public float JumpSpeed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MovingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-MovingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpSpeed);
+        }
+    }
+}
